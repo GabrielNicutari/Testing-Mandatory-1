f6734dfe381a533519b90cd890f509e5d00cfffc
Marianna <mana998@azet.sk>
2022-03-17 21:10:03 +0100

    Added initial generateStreet function

--- src/functions/address.js
+++ src/functions/address.js
@@ -27,4 +27,17 @@ router.get('/address', (req, res) => {
   // });
 });
 
+function generateStreet() {
+  let street = '';
+  do {
+      street += getRandomCharacter()
+  } while(Math.random() < 0.5 && street.length < 50);
+  return street;
+}
+
+function getRandomCharacter() {
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  return possible.charAt(Math.floor(Math.random() * possible.length));
+}
+
 module.exports = {router};
