1eb6a441aaa58a3324b3e01e152e0be421414fdd
Marianna <mana998@azet.sk>
2022-03-17 21:10:35 +0100

    Added initial generateStreetNumber function

--- src/functions/address.js
+++ src/functions/address.js
@@ -35,9 +35,20 @@ function generateStreet() {
   return street;
 }
 
+function generateStreetNumber() {
+    let streetNumber = getRandomNumber(1, 999);
+    if (Math.random() < 0.5) streetNumber += getRandomCharacter().toUpperCase();
+    return streetNumber;
+}
+
 function getRandomCharacter() {
-  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  return possible.charAt(Math.floor(Math.random() * possible.length));
+const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+return possible.charAt(Math.floor(Math.random() * possible.length));
+}
+
+//both values included
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 module.exports = {router};
