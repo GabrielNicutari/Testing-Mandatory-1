4dd78aad2003347e5e0f1c9678d02e62dea4ef9c
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-03-17 10:52:50 +0100

    App.js cleanup

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const app = express();
 // setup static dir
 app.use(express.static(`${__dirname}`));
 
-// allows to recognise incoming object as json object
-app.use(express.json());
-
-// allow to pass form data
-app.use(express.urlencoded({ extended: true }));
-
 const PORT = process.env.PORT || 8080;
 /* eslint-disable no-debugger, no-console */
 app.listen(PORT, (error) => {
