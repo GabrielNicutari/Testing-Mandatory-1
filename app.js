81ce28fa9a6a8362a06c99df19e67eecd3cdddb9
dprzygocka <przygocka.dagmara@gmail.com>
2022-03-17 12:24:17 +0100

    eslint

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 const express = require('express');
 
 const app = express();
-
+console.log('hi');
 // setup static dir
 app.use(express.static(`${__dirname}`));
 
 const PORT = process.env.PORT || 8080;
 /* eslint-disable no-debugger, no-console */
 app.listen(PORT, (error) => {
-    if (error) {
-      console.log(error);
-    }
-    console.log('Server is running on port', Number(PORT));
+  if (error) {
+    console.log(error);
+  }
+  console.log('Server is running on port', Number(PORT));
 });
