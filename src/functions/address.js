ad8e899e44d1b4c0230fa3cd93729bac3ad4651e
Cristian Ou <mccrys_00@yahoo.com>
2022-03-17 14:44:14 +0100

    Test DB with a dump query

--- src/functions/address.js
+++ src/functions/address.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
+const dbPool = require('../database/connection').pool;
+
 
 router.get('/address', (req, res) => {
+
+  dbPool.getConnection(function(err, conn) {
+    if (err) console.log(err);
+    
+    // Do something with the connection
+    conn.query("SELECT * FROM postal_code LIMIT 5", (err, rows, fields) => {
+      if (err) console.log(err);
+      console.log(rows);
+    });
+  
+    // Don't forget to release the connection when finished!
+    dbPool.releaseConnection(conn);
+
+    // There might be queries that are not ended and still be displayed randomly, 
+    // so it's better to end the pool at some point. Im not sure where exactly to place the function, though.
+  });
   res.send("Hello world!");
+
+  // dbPool.end(function (err) {
+  //   if (err) throw err;
+  //   // process.exit();
+  // });
 });
 
 module.exports = {router};
