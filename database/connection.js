55d64f72679acba9ef49210875ab8e8c5f4e98c1
dprzygocka <przygocka.dagmara@gmail.com>
2022-03-17 10:19:01 +0100

    local db

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const pool  = mysql.createPool(
-  process.env.CLEARDB_DATABASE_URL, {
-  connectionLimit : 5,
+const pool  = mysql.createPool({
+    host     : process.env.HOST,
+    database : process.env.DATABASE,
+    user     : process.env.USER,
+    password : process.env.PASSWORD,
+    connectionLimit : 5,
 });
 
 module.exports = {
