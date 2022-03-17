a7fb549a4d6b5616238035c3a682ae9d3496b371
dprzygocka <przygocka.dagmara@gmail.com>
2022-03-17 10:08:25 +0100

    init set up

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const pool  = mysql.createPool(
+  process.env.CLEARDB_DATABASE_URL, {
+  connectionLimit : 5,
+});
+
+module.exports = {
+  pool,
+};
