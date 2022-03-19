2e4fc8bedcb07513406e8b32adcb820e2d635017
Cristian Ou <mccrys_00@yahoo.com>
2022-03-19 11:43:18 +0100

    Clean up

--- src/functions/address.js
+++ src/functions/address.js
@@ -15,16 +15,9 @@ router.get('/address', (req, res) => {
   
     // Don't forget to release the connection when finished!
     dbPool.releaseConnection(conn);
-
-    // There might be queries that are not ended and still be displayed randomly, 
-    // so it's better to end the pool at some point. Im not sure where exactly to place the function, though.
   });
   res.send("Hello world!");
 
-  // dbPool.end(function (err) {
-  //   if (err) throw err;
-  //   // process.exit();
-  // });
 });
 
 module.exports = {router};
