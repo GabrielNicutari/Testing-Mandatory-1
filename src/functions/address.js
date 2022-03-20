b2e95c7e33477092148ce98a491963228bcc59b3
Marianna <mana998@azet.sk>
2022-03-20 11:33:02 +0100

    Added lower limit for street length

--- src/functions/address.js
+++ src/functions/address.js
@@ -1,48 +1,69 @@
 const router = require("express").Router();
-const dbPool = require('../database/connection').pool;
+const promisePool = require('../database/connection').pool.promise();
 
+async function getRandomPostalCodeAndTown() {
+  const mysqlQuery = "SELECT * FROM postal_code ORDER BY RAND() LIMIT 1";
+  const [rows, fields] = await promisePool.query(mysqlQuery);
+  return rows;
+  // https://stackoverflow.com/questions/57121227/why-do-we-need-to-release-connection-when-using-connection-pool-in-mysql
+}
 
-router.get('/address', (req, res) => {
+//Returns a random integer from 'start' to array.length - 1 + start
+function getRandomNumber(maxLength, start = 0) {
+  return Math.floor(Math.random() * (maxLength - start)) + start;
+}
 
-  dbPool.getConnection(function(err, conn) {
-    if (err) console.log(err);
+function getRandomDoor() {
+  const doorOptions = ['side', 'number', 'house-number']; 
+  const options = { 
+    'SIDE': doorOptions[0],
+    'NUMBER': doorOptions[1], 
+    'HOUSE_NUMBER': doorOptions[2]
+  };
+  const randOptionIndex = getRandomNumber(doorOptions.length);
+
+  switch(doorOptions[randOptionIndex]) {
+    case options.SIDE:
+      const sideOptions = ['th', 'mf', 'tv'];
+      const randOptionIndex = getRandomNumber(sideOptions.length); 
+      return sideOptions[randOptionIndex];
     
-    // Do something with the connection
-    conn.query("SELECT * FROM postal_code LIMIT 5", (err, rows, fields) => {
-      if (err) console.log(err);
-      console.log(rows);
-    });
-  
-    // Don't forget to release the connection when finished!
-    dbPool.releaseConnection(conn);
+    case options.NUMBER:
+      return getRandomNumber(50, 1);
+    
+    case options.HOUSE_NUMBER:
+      let result = '';
+      const firstAsciiCh = 97;
+      const alphabetLength = 26;
+      result += String.fromCharCode(firstAsciiCh + getRandomNumber(alphabetLength)); // Returns a random character from the english alphabet
 
-    // There might be queries that are not ended and still be displayed randomly, 
-    // so it's better to end the pool at some point. Im not sure where exactly to place the function, though.
-  });
-  res.send("Hello world!");
+      if (getRandomNumber(2)) result += '-';
 
-  // dbPool.end(function (err) {
-  //   if (err) throw err;
-  //   // process.exit();
-  // });
-});
+      result += getRandomNumber(999, 1);
+      return result; // eg: c-1, f23, z-999
+    
+    default:
+      'Internal Error';
+  }
+}
 
 function generateStreet() {
     let street = '';
-    do {
+    const length = getRandomNumber(51, 10);
+    while (street.length < length) {
         street += getRandomCharacter()
-    } while(Math.random() < 0.5 && street.length < 51);
+    };
     return street;
 }
 
 function generateStreetNumber() {
-    let streetNumber = getRandomNumber(1, 1000);
+    let streetNumber = getRandomNumber(1000, 1);
     if (Math.random() < 0.5) streetNumber += getRandomCharacter().toUpperCase();
     return streetNumber;
 }
 
 function generateFloor() {
-    let floor = getRandomNumber(0, 100);
+    let floor = getRandomNumber(100);
     if (floor === 0) floor = 'st';
     return floor;
 }
@@ -52,8 +73,16 @@ function getRandomCharacter() {
     return possible.charAt(Math.floor(Math.random() * possible.length));
 }
 
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
+router.get('/address', async (req, res) => {
+  const postalCodeAndTown = await getRandomPostalCodeAndTown();
+  res.send({
+    street: generateStreet(),
+    streetFloor: generateStreetNumber(),
+    floor: generateFloor(),
+    door: getRandomDoor(),
+    postalCode: postalCodeAndTown[0].cPostalCode,
+    town: postalCodeAndTown[0].cTownName
+  });
+});
 
 module.exports = {router};
