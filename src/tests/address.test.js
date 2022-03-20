c83838697c946cfd1c9fa49f240581794be4adbb
Marianna <mana998@azet.sk>
2022-03-20 11:12:28 +0100

    Added test file with description

---
+++ src/tests/address.test.js
@@ -0,0 +1,8 @@
+const {generateFloor, generateStreet, generateStreetNumber} = require('../functions/address');
+
+//generateStreet
+//length should be between 1-50 characters
+//only contains a-zA-Z + danish characters
+//everything is generated randomly - hard to test, no parameters passed
+//can try to run it in a loop multiple times and check for conditions
+//but it won't catch the problem every time in case it is there
