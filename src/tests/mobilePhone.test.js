a594d2756abb559a70c435ab08215adfdb33113c
dprzygocka <przygocka.dagmara@gmail.com>
2022-03-20 10:20:13 +0100

    improve test

--- src/tests/mobilePhone.test.js
+++ src/tests/mobilePhone.test.js
@@ -2,13 +2,15 @@ const { digitsToArray, getRandomPhoneNumber, getRandomNumber } = require('../fun
 describe('mobile phone', function(){
     it('get random number up to maximum value', () => {
         const maxNumber = 10;
+        expect(getRandomNumber(maxNumber)).not.toBeNaN();
         expect(getRandomNumber(maxNumber)).toBeGreaterThanOrEqual(0);
         expect(getRandomNumber(maxNumber)).toBeLessThanOrEqual(maxNumber-1);
     }); 
     
-    it('check if number starts from digit from list of possible digits for the danish number', () => {
+    it('check getRandomPhoneNumber() if number starts from digit from list of possible digits for the danish number', () => {
         let phoneNumber = getRandomPhoneNumber();
         const digitsArray = digitsToArray();
+        expect(String.isString(phoneNumber)).toBe(true);
         expect(phoneNumber).toMatch(new RegExp(`^(${digitsArray.join("|")})`));
     }); 
 
