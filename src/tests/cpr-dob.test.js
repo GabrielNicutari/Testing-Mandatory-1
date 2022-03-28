940cbbfa6d44bcef5e2c7dc780448858115e59e1
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-03-28 17:50:48 +0200

    uncommented test

--- src/tests/cpr-dob.test.js
+++ src/tests/cpr-dob.test.js
@@ -5,21 +5,21 @@ describe('cpr and dob test suite', () => {
   const { cpr: maleCpr } = generateCPR('male');
   const { cpr: femaleCpr  } = generateCPR('female');
 
-  it('should generate a valid male cpr', () => {
+  test('should generate a valid male cpr', () => {
     expect(maleCpr).toHaveLength(10);
     const lastDigit = maleCpr.charAt(maleCpr.length - 1);
     const isLastDigitOdd = lastDigit % 2 !== 0;
     expect(isLastDigitOdd).toBe(true);
   });
 
-  // it('Should generate a valid female cpr', () => {
-  //   expect(femaleCpr).toHaveLength(10);
-  //   const lastDigit = femaleCpr.charAt(femaleCpr.length - 1);
-  //   const isLastDigitEven = lastDigit % 2 === 0;
-  //   expect(isLastDigitEven).toBe(true);
-  // });
+  test('Should generate a valid female cpr', () => {
+    expect(femaleCpr).toHaveLength(10);
+    const lastDigit = femaleCpr.charAt(femaleCpr.length - 1);
+    const isLastDigitEven = lastDigit % 2 === 0;
+    expect(isLastDigitEven).toBe(true);
+  });
 
-  it('Date of birth should match the cpr date', () => {
+  test('Date of birth should match the cpr date', () => {
     // eslint-disable-next-line
     const cpr_dob = maleCpr.substring(0, 6);
     const { dob } = getDateOfBirth(maleCpr);
